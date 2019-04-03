1325121bacccf1dcaf55a855fcedb14d9eb2a9de
Simokod <44751590+Simokod@users.noreply.github.com>
2019-04-03 12:58:54 +0300

    Add files via upload

---
+++ q3.ts
@@ -0,0 +1,56 @@
+import { map, zipWith } from "ramda";
+import { CExp, Parsed, PrimOp, AppExp, LitExp, isEmpty, makeStrExp, parseL3, IfExp, DefineExp, Program, Binding } from "./imp/L3-ast";
+import { makeAppExp, makeDefineExp, makeIfExp, makeProcExp, makeProgram, makePrimOp, makeLetExp, makeBinding, makeLitExp } from "./imp/L3-ast";
+import { isAppExp, isAtomicExp, isCExp, isDefineExp, isIfExp, isLetExp, isLitExp, isPrimOp, isProcExp, isProgram } from "./imp/L3-ast";
+import {isError} from './imp/error';
+import { makeEmptySExp, isEmptySExp, isCompoundSExp } from "./imp/L3-value";
+import {first, second, rest} from './imp/list';
+import { unparseL3 } from "./imp/L3-unparse";
+/*
+Purpose: @TODO
+Signature: @TODO
+Type: @TODO
+*/
+export const l3ToL30 = (exp: Parsed | Error): Parsed | Error  =>
+   isProgram(exp) ? handleProgram(exp) :
+   isDefineExp(exp) ? handleDefineExp(exp) :
+   isCExp(exp) ? handleCExp(exp) :
+   exp;
+   
+export const handleProgram = (exp: Program): Program =>
+   map(l3ToL30, exp.exps);
+
+export const handleDefineExp = (exp: DefineExp): Parsed | Error =>
+   isAppExp(exp.val) ? makeDefineExp(exp.var, handleAppExp(exp.val)) :
+   isLitExp(exp.val) ? makeDefineExp(exp.var, handleLitExp(exp.val)) :
+   exp;
+
+export const handleCExp = (exp: CExp): CExp =>
+   isAtomicExp(exp) ? exp :
+   isAppExp(exp) ? handleAppExp(exp) :
+   isIfExp(exp) ? 
+      makeIfExp(handleCExp(exp.test), handleCExp(exp.then), handleCExp(exp.alt)) :
+   isProcExp(exp) ?
+      makeProcExp(exp.args, map(l3ToL30, exp.body)) :
+   isLetExp(exp) ?
+      makeLetExp(exp.bindings, map(l3ToL30, exp.body)) :
+   isLitExp(exp) ? rewriteLitExp(exp) : exp;
+
+export const handleAppExp = (exp: AppExp): CExp => 
+   isPrimOp(exp.rator) && exp.rator.op === "list" ?  
+   isEmpty(exp.rands) ? makeLitExp(makeEmptySExp()) :
+   makeAppExp(makePrimOp("cons"),[handleCExp(first(exp.rands)), rewriteAppExp(rest(exp.rands))]) :
+   exp;
+
+export const rewriteAppExp = (exp: CExp[]): CExp =>
+   isEmpty(exp) ? makeLitExp(makeEmptySExp()) :
+   makeAppExp(makePrimOp("cons"), [handleCExp(first(exp)), rewriteAppExp(rest(exp))]);
+
+
+export const handleLitExp = (exp: LitExp): CExp =>
+   exp;
+   
+export const rewriteLitExp = (exp: LitExp): CExp =>
+   exp;
+
+   console.log(parseL3(`(let ((x '(1 2)) (y (list 3 4))) (list x y))`));
